80f8e027853c10d307da25f0345ac097bf6f54da
McCorney Severin <mccorney.severin@gmail.com>
2020-11-24 10:25:31 -0800

    Adding verify.

---
+++ lib/Analyze/tests/ParserTest.cpp
@@ -0,0 +1,13 @@
+#include "stone/Core/SrcMgr.h"
+#include "gtest/gtest.h"
+
+using namespace stone; 
+
+class ParserTest : public ::testing::Test {
+public:
+
+};
+
+TEST_F(ParserTest, ParseFile) {
+
+}
---
+++ lib/Core/tests/DiagTest.cpp
@@ -0,0 +1,13 @@
+#include "stone/Core/Diag.h"
+#include "gtest/gtest.h"
+
+using namespace stone; 
+
+class DiagTest : public ::testing::Test {
+public:
+
+};
+
+TEST_F(DiagTest, Diagnose) {
+
+}
