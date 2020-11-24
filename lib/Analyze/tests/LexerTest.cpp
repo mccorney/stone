a9e8b9de99d81fa55bf330fe5dea6035f71ffa7d
McCorney Severin <mccorney.severin@gmail.com>
2020-11-24 00:49:40 -0800

    Adding Tests.

---
+++ lib/Analyze/tests/LexerTest.cpp
@@ -0,0 +1,13 @@
+#include "stone/Core/SrcMgr.h"
+#include "gtest/gtest.h"
+
+using namespace stone; 
+
+class LexerTest : public ::testing::Test {
+public:
+
+};
+
+TEST_F(LexerTest, GetNextToken) {
+
+}
