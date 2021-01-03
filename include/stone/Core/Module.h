f4c14480f1c683371f480fffb42ce9d5685a768a
McCorney Severin <mccorney.severin@gmail.com>
2021-01-02 20:43:44 -0800

    Updating module.

--- include/stone/Core/Module.h
+++ include/stone/Core/Module.h
@@ -1,26 +1,52 @@
 #ifndef STONE_CORE_MODULE_H
 #define STONE_CORE_MODULE_H
 
+#include "stone/Core/ASTContext.h"
 #include "stone/Core/Decl.h"
 #include "stone/Core/Identifier.h"
-#include "stone/Core/ASTContext.h"
+#include "stone/Core/ASTScope.h"
+#include "stone/Core/LLVM.h"
 
 #include "llvm/ADT/SmallVector.h"
 
 namespace stone {
 namespace syntax {
+class Module;
 
 class ModuleUnit {
 public:
   enum class Kind { Source, Builtin };
 
+private:
+  ModuleUnit::Kind kind;
+
+public:
+  ModuleUnit(ModuleUnit::Kind kind, Module &owner) : kind(kind) {}
+
+public:
+  ModuleUnit::Kind GetKind() const { return kind; }
+
 public:
 };
 
 class SourceUnit final : public ModuleUnit {
+private:
+	friend ASTContext;
+	bool isMain; 
+	//llvm::NullablePtr<ASTScope> scope = nullptr;
+
 public:
-  enum class Kind { Stone };
+  enum class Kind { Library };
 public:
+  SourceUnit::Kind kind;
+
+public:
+  SourceUnit(Module &owner, SourceUnit::Kind kind, bool isMain = false);
+  ~SourceUnit();
+
+  static bool classof(const ModuleUnit *unit) {
+    return unit->GetKind() == ModuleUnit::Kind::Source;
+  }
 };
 
 class BuiltinUnit final : public ModuleUnit {
@@ -29,9 +55,8 @@ public:
 
 class Module final : public DeclContext, public TypeDecl {
 private:
-	Module(Identifier name, ASTContext &astContext);
+  Module(Identifier name, ASTContext &astContext);
 
-public:
   llvm::SmallVector<ModuleUnit *, 2> units;
 
 public:
@@ -43,7 +68,12 @@ public:
     return {units.begin(), units.size()};
   }
   void AddUnit(ModuleUnit &unit);
+
+  SourceUnit &GetMainSourceUnit() const;
+
+  ModuleUnit &GetMainUnit(ModuleUnit::Kind kind) const;
 };
+
 } // namespace syntax
 } // namespace stone
 
--- lib/Core/Module.cpp
+++ lib/Core/Module.cpp
@@ -1,1 +1,26 @@
 #include "stone/Core/Module.h"
+#include "llvm/ADT/DenseMap.h"
+#include "llvm/ADT/DenseSet.h"
+#include "llvm/ADT/STLExtras.h"
+#include "llvm/ADT/SmallPtrSet.h"
+#include "llvm/ADT/StringExtras.h"
+#include "llvm/ADT/TinyPtrVector.h"
+#include "llvm/Support/MD5.h"
+#include "llvm/Support/MemoryBuffer.h"
+#include "llvm/Support/Path.h"
+#include "llvm/Support/SaveAndRestore.h"
+#include "llvm/Support/raw_ostream.h"
+
+using namespace stone::syntax;
+
+void Module::AddUnit(ModuleUnit &unit) {
+  // If this is a LoadedFile, make sure it loaded without error.
+  // assert(!(isa<LoadedFile>(newFile) &&
+  //         cast<LoadedFile>(newFile).hadLoadError()));
+  // Require Main and REPL files to be the first file added.
+  assert(units.empty() || !isa<SourceUnit>(unit) ||
+         cast<SourceUnit>(unit).kind == SourceUnit::Kind::Library
+         /*||cast<SourceUnit>(unit).Kind == SourceUnit::Kind::SIL*/);
+  units.push_back(&unit);
+  // ClearLookupCache();
+}
