3110bee26c8653fecee21f7511de64e1ef3aee4f
McCorney Severin <mccorney.severin@gmail.com>
2021-01-02 19:58:01 -0800

    Updating module

--- include/stone/Core/Module.h
+++ include/stone/Core/Module.h
@@ -3,15 +3,21 @@
 
 #include "stone/Core/Decl.h"
 
+#include "llvm/ADT/SmallVector.h"
+
 namespace stone {
 namespace syntax {
 
 class ModuleUnit {
 public:
   enum class Kind { Source, Builtin };
+
+public:
 };
 
 class SourceUnit final : public ModuleUnit {
+public:
+  enum class Kind { Stone };
 public:
 };
 
@@ -21,6 +27,17 @@ public:
 
 class Module final : public DeclContext, public TypeDecl {
 public:
+  llvm::SmallVector<ModuleUnit *, 2> units;
+
+public:
+  llvm::ArrayRef<ModuleUnit *> GetUnits() {
+    assert(!units.empty());
+    return units;
+  }
+  llvm::ArrayRef<const ModuleUnit *> GetUnits() const {
+    return {units.begin(), units.size()};
+  }
+  void AddUnit(ModuleUnit &unit);
 };
 } // namespace syntax
 } // namespace stone
